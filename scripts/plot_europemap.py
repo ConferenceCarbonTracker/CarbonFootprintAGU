3b3ebf35cc39944e1f73551463c915d3a0d258ef
milankl <milankloewer@gmx.de>
2019-11-24 18:47:04 +0000

    More beautiful colors on maps

--- scripts/plot_europemap.py
+++ scripts/plot_europemap.py
@@ -25,15 +25,15 @@ fig = plt.figure(figsize=(6,6))
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.LambertConformal(central_longitude=180+SFOloc[1]))
 ax.set_extent([-13, 36, 40, 64], crs=ccrs.Geodetic())
 
-ax.add_feature(cfeature.OCEAN,facecolor="#97BACD",zorder=-2)
-ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.OCEAN,facecolor="#4381a2",zorder=-2)
+ax.add_feature(cfeature.LAND,facecolor="0.9")
 ax.coastlines(resolution="50m")
 ax.add_feature(cfeature.BORDERS,lw=0.5,alpha=1)
-ax.add_feature(cfeature.LAKES,facecolor="#97BACD",zorder=0.1)
+ax.add_feature(cfeature.LAKES,facecolor="#4381a2",zorder=0.1)
 
 # rectangle to hide shetland
 patch = [Rectangle((-3.5e6,3.8e6),2e5,2e5)]
-p = PatchCollection(patch,facecolor="#97BACD",zorder=2)
+p = PatchCollection(patch,facecolor="#4381a2",zorder=2)
 ax.add_collection(p)
 
 ax.text(0.5,0.92,"San Francisco",alpha=0.5,transform=ax.transAxes,ha="center",va="top")
@@ -41,17 +41,17 @@ parrow = [Arrow(-3.3e6,4.05e6,0,2e5,width=2e5)]
 ax.add_collection(PatchCollection(parrow,color="lightgreen",alpha=0.9))
 
 colr = "#D71455"
-vmax = 1
+vmax = 0.9
 nmax = df["N"].max()*0.2
-cmap = cmo.haline_r
-ax.scatter(df["lon"],df["lat"],0.2+7*np.sqrt(df["N"]),
+cmap = cmo.thermal_r
+ax.scatter(df["lon"],df["lat"],0.1+7*np.sqrt(df["N"]),
         c=cmap(((df["N"]/nmax)**(1/4))*vmax), transform=ccrs.Geodetic(),
         alpha=.9,edgecolor="k",lw=0.3,zorder=2)
 
 # for legend
 nums = [1,10,50,250]
 for num in nums:
-    ax.scatter(0,0,0.2+7*np.sqrt(num),c=[cmap(((num/nmax)**(1/4))*vmax)],
+    ax.scatter(0,0,0.1+7*np.sqrt(num),c=[cmap(((num/nmax)**(1/4))*vmax)],
         label="{:d}".format(num),transform=ccrs.Geodetic(),edgecolor="k",lw=0.3)
 
 ax.set_title("Attendees of AGU Fall Meeting 2019",loc="left",fontweight="bold")
